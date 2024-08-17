800ecf4c20a93f41e0beef48e8ee384592b61faa
shubham9624 <shubhamkrissh88@gmail.com>
2024-08-17 12:40:58 +0530

    Product Application

---
+++ Model/user.model.js
@@ -0,0 +1,16 @@
+export default class UserModel{
+constructor(username,email,password){
+    this.username=username;
+    this.email=email;
+    this.password=password;
+}
+static addUser(username,email,password){
+    const user=new UserModel(username,email,password);
+   users.push(user);
+}
+static isValidUser(username,password){
+    const result=users.find(user => user.username===username && user.password===password);
+    return result;
+}
+}
+const users=[];
