658051591581b0fad2ce1376c5bd2e0a58ba7198
shubham9624 <shubhamkrissh88@gmail.com>
2024-08-17 12:49:03 +0530

    Revert "Product Application"
    This reverts commit 800ecf4c20a93f41e0beef48e8ee384592b61faa.

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-import express from 'express';
-import path from 'path';
-import ejslayout from 'express-ejs-layouts';
-import ProductController from './Controllers/Product.controller.js';
-import {upload} from './Controllers/Product.controller.js';
-import UserController from './Controllers/user.controller.js';
-const server = express();
-const productController = new ProductController();
-const usercontroller=new UserController();
-server.use(ejslayout);
-server.set('view engine',"ejs");
-server.set('Views',path.resolve('Views','Product.html'));
-// Serve static files from the Views directory
-server.use(express.static(path.resolve('Views')));
-server.use(express.urlencoded({extended: true}));
+const http =  require('http');
 
-// Route for serving the Product.html file
-server.get('/', productController.getProduct) ;
-server.get('/addProduct',productController.addProducts);
-server.get('/update-product/:id',productController.getUpdateProduct);
-server.post('/',upload.single("imageUrl") ,productController.postProducts);
-server.post('/update-product',productController.postUpdateProduct);
-server.get('/delete-product/:id',productController.deleteProduct);
-server.get('/register',usercontroller.getRegister);
-server.post('/register',usercontroller.postregister);
-server.get('/login',usercontroller.getLogin);
-server.post('/login',usercontroller.postlogin);
+const server = http.createServer((req, res) => {
+    if(req.url== '/about'){
+        return res.end("<h1>about page</h1>");
+    }
+    else if(req.url=='/home')
+    {
+        return res.end("<h1>home page</h1>");
+    }
+    res.end("<h1>page</h1>");
+});
+
 server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+    console.log("Server is running on port 3000");
 });
